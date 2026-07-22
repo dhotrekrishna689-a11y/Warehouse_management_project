77f042cd86017fbd01613cc761444bfee2db6d86
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2026-07-23 00:02:52 +0530

    Updated app.py & Tables created in pgadmin4

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db.init_app(app)
 
+from models.category import Category
+from models.product import Product
+from models.batch import Batch
+from models.rack import Rack
+from models.inventory import Inventory
+from models.user import User
+from models.shipment import Shipment
+from models.shipmentitem import ShipmentItem
+from models.order import Order
+from models.orderitem import OrderItem
+from models.stockmovement import StockMovement
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route("/")
 def home():
     return "Smart Warehouse Bakced Running..."
