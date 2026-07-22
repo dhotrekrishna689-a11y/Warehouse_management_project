4899a23148ea3b9e80d0c9e2e08a75a5f026d770
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2026-07-22 20:09:52 +0530

    Adding models/order.py

---
+++ models/order.py
@@ -0,0 +1,21 @@
+from database.db_instance import db
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    order_id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    order_number = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    order_date = db.Column(
+        db.Date,
+        nullable=False
+    )
