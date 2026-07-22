971f055d082f482eeee49d437dd2d93e8da098b4
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2026-07-22 20:13:28 +0530

    Adding models/stockmovement.py

---
+++ models/stockmovement.py
@@ -0,0 +1,37 @@
+from database.db_instance import db
+
+
+class StockMovement(db.Model):
+    __tablename__ = "stock_movements"
+
+    stock_movement_id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    inventory_id = db.Column(
+        db.Integer,
+        db.ForeignKey("inventory.inventory_id"),
+        nullable=False
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.user_id"),
+        nullable=False
+    )
+
+    quantity_changed = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    movement_type = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    movement_date = db.Column(
+        db.Date,
+        nullable=False
+    )
