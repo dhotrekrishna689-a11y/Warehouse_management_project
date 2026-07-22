d4888ee3213a2dbeec31abbc8aa5aa06a1bc0c25
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2026-07-22 20:06:50 +0530

    Adding models/shipment.py

---
+++ models/shipment.py
@@ -0,0 +1,27 @@
+from database.db_instance import db
+
+
+class Shipment(db.Model):
+    __tablename__ = "shipments"
+
+    shipment_id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    shipment_number = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    received_date = db.Column(
+        db.Date,
+        nullable=False
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.user_id"),
+        nullable=False
+    )
