082848f4d3633a22be91e946bd1984b6c8714aee
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2026-07-27 20:20:49 +0530

    Created 3 api routes()

--- routes/categories_routes.py
+++ routes/categories_routes.py
@@ -18,3 +18,15 @@ def create_catgories():
 @category_bp.route("", methods=["GET"])
 def get_category():
     return categories_controllers.get_category()
+
+@category_bp.route("/<int:category_id>", methods=["GET"])
+def get_specific_category(category_id):
+    return categories_controllers.get_specific_category(category_id)
+
+@category_bp.route("<int:category_id>", methods=["PUT"])
+def update_category(category_id):
+    return categories_controllers.update_category(category_id)
+
+@category_bp.route("<int:category_id>", methods=["DELETE"])
+def delete_category(category_id):
+    return categories_controllers.delete_category(category_id)
