864f4dc29d59c75241e4d6e0beb8746ac4f123d5
Krishnadhotre3/Kd <dhotrekrishna689@gmail.com>
2026-07-27 20:19:57 +0530

    Created 3 api Contoller function

--- controllers/categories_controllers.py
+++ controllers/categories_controllers.py
@@ -42,3 +42,83 @@ def get_category():
             "message": "Categories fetched successfully",
             "data": converted_category
     }), 200
+
+
+def get_specific_category(category_id):
+
+    get_specific_cat = categories_services.get_specific_category(category_id)
+
+    if get_specific_cat is None:
+        return jsonify({
+                    "message":"Category Not exists"
+                }), 404
+
+    return jsonify({
+        "message":"Category found",
+        "data": get_specific_cat.to_dict()
+    }), 200
+
+from flask import request, jsonify
+from services import categories_services
+
+
+def update_category(category_id):
+
+    # Request receive
+    data = request.get_json()
+
+    # Data extract
+    name = data.get("name")
+
+    # Service call
+    update_cat = categories_services.update_category(name, category_id)
+
+    # Category not found
+    if update_cat is None:
+        return jsonify({
+            "message": "Category not found"
+        }), 404
+
+    # Duplicate category
+    if update_cat == "duplicate":
+        return jsonify({
+            "message": "Category already exists"
+        }), 409
+
+    # Success
+    return jsonify({
+        "message": "Category updated successfully",
+        "data": update_cat.to_dict()
+    }), 200
+
+
+
+
+from flask import jsonify
+from services import categories_services
+
+
+def delete_category(category_id):
+
+    # Service call
+    del_cat = categories_services.delete_category(category_id)
+
+    # Category not found
+    if del_cat is None:
+        return jsonify({
+            "message": "Category not found"
+        }), 404
+
+    # Category is in use
+    if del_cat == "Cannot delete, beacause product already using this category":
+        return jsonify({
+            "message": "Cannot delete category because it is assigned to one or more products."
+        }), 409
+
+    # Success
+    return jsonify({
+        "message": "Category deleted successfully",
+        "data": del_cat.to_dict()
+    }), 200
+
+    
